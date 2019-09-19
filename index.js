f39e2612c1a53ae4c9a51c6f48019472e0810cbb
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-19 05:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 // Code your solution in this file
-// function findMatching(driversArray, string) {
-//   return driversArray.filter(function (driverElement) {
-//     return driverElement.toLowerCase() === string.toLowerCase();
-//   });
-// }
-
-// function fuzzyMatch(driversArray, string) {
-//   return driversArray.filter (function (driverElement) {
-//     return driverElement.slice(0, string.length) === string
-//   })
-// }
-
-// function matchName(driversArray, string) {
-//   return driversArray.filter (function (driverElement) {
-//     return driverElement.name === string
-//     // return driverElement['name'] === string
-//   })
-// }
 
 function findMatching(array, string) {
   return array.filter(function(driver) {
